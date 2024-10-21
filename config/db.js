fffe82376790f562a7be464213290729f7ade5e5
Talenthubrecruitment <hendrysakdiyah@gmail.com>
2024-10-22 04:53:31 +0900

    fix:DB information

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 // Create a connection pool to the MySQL database
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD || '',
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: "69.167.168.82",
+    user: "dxbee_ktap2",
+    password: "ktapktapktap2",
+    database: "dxbee_ktap2",
+    port: 3306
 });
 
 // Export the promise-based pool for easy usage later
